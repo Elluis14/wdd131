555d8d9dcc826e8c244aff55f7487679d6977a65
Elluis14 <95771375+Elluis14@users.noreply.github.com>
2025-07-03 16:29:09 -0700

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// Show current year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Show last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
